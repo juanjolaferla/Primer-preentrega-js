3bbbb2713080e660ca45034d6d4bd3ba39209e1b
Juan Laferla <jlaferla@cemic.edu.ar>
2023-03-31 13:45:32 -0300

    creaccion repo

---
+++ main.js
@@ -0,0 +1,45 @@
+
+/*let porcentaje = parseFloat (prompt("¿Que porcentaje de descuente quieres aplicar?"));
+console.log( "--> Porcentaje ingresado", porcentaje);
+let precio = parseFloat (prompt("¿Cuanto Cuesta tu producto?"));
+let descuento = 0;
+let total = precio;
+if (porcentaje > 0) {
+descuento = precio * (porcentaje/100)
+}
+total = precio - descuento;0
+console.log("--> El valor final del producto es de " + total);*/
+
+
+/*let numero = parseFloat (prompt("Ingresa el monto deseado hasta $500000"));
+let cuotas = parseInt (prompt ("Cantidad de cuotas"));
+let porcentaje ;
+
+switch(cuotas){
+    case 3:
+    porcentaje = 20;
+    
+    break;
+    case 6:
+    porcentaje = 40;
+    calcularPorcentaje(numero , porcentaje)
+    break;
+    case 12:
+    porcentaje = 60;
+    calcularPorcentaje(numero , porcentaje)
+    break;
+}
+
+function calcularPorcentaje (numero,porcentaje){
+    return numero / 100 * porcentaje;
+}
+alert  (calcularPorcentaje(numero , porcentaje));
+
+total = (calcularPorcentaje(numero , porcentaje)) + numero;
+
+totalCuota= total / cuotas;
+ 
+
+console.log("--> El valor final del credito " + total);
+console.log("--> El valor final de cuota " + totalCuota);*/
+
